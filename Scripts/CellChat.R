dc64f7107622ab8f1b7d798dd5c12bef57b08546
tbehrenso <thomas.behrens@student.unibe.ch>
2025-06-20 09:38:49 +0200

    Small CellChat changes

--- Scripts/CellChat.R
+++ Scripts/CellChat.R
@@ -2,7 +2,11 @@ library(NMF)
 library(circlize)
 library(ComplexHeatmap)
 library(CellChat)
+library(Seurat)
 
+SYN_merged <- readRDS('SYN_merged_FINALv4.RData')
+SYN_merged <- FindClusters(SYN_merged, resolution = 2.2)
+SYN_merged$RNA_snn_res.1 <- NULL
 
 # Split Seurat Object
 SYN_split <- SplitObject(SYN_merged, split.by = 'orig.ident')
@@ -10,15 +14,16 @@ SYN_split <- SplitObject(SYN_merged, split.by = 'orig.ident')
 SYN_SNCA <- SYN_split[[1]]
 SYN_PLX <- SYN_split[[2]]
 SYN_PLX <- subset(SYN_PLX, idents='42', invert=T)  # remove cluster 42 (no corresponding cells in SNCA condition)
+SYN_PLX <- subset(SYN_PLX, idents='Erythrocytes', invert=T)
 
 # rename clusters (for CellChat, a cluster cannot be called just '0')
 SYN_SNCA$clusters_renamed <- paste0('c',SYN_SNCA$seurat_clusters)
 SYN_PLX$clusters_renamed <- paste0('c',SYN_PLX$seurat_clusters)
 
-# manually start from a Seurat object (not used)
-data.input <- SYN_merged[["RNA"]]$data # normalized data matrix
-labels <- Idents(SYN_merged)
-meta <- data.frame(group = labels, row.names = names(labels)) # create a dataframe of the cell labels
+# # manually start from a Seurat object (not used)
+# data.input <- SYN_merged[["RNA"]]$data # normalized data matrix
+# labels <- Idents(SYN_merged)
+# meta <- data.frame(group = labels, row.names = names(labels)) # create a dataframe of the cell labels
 
 
 # directly create CellChat object
@@ -30,7 +35,7 @@ cellChat_PLX <- createCellChat(object = SYN_PLX, group.by = "clusters_renamed", 
 # ---------------------------------------------------------------
 
 # SELECT SAMPLE
-cellChat <- cellChat_SNCA
+cellChat <- cellChat_PLX
 
 # Set ligand-receptor interaction database
 CellChatDB <- CellChatDB.mouse      
@@ -54,7 +59,7 @@ cellchat <- smoothData(cellchat, adj = PPI.mouse)
 
 ### Inference of Cell-Cell Communication Network
 
-cellchat <- computeCommunProb(cellchat, type = "triMean")
+cellchat <- computeCommunProb(cellchat, type = "triMean", population.size=F)  # by default, requires 10 cells per group
 
 # Change order of groups
 # cellchat <- updateClusterLabels(cellchat, new.order = paste0('c',0:41))
@@ -97,12 +102,15 @@ par(mfrow=c(1,1))
 netVisual_aggregate(cellchat, signaling = 'CXCL', layout = "circle")
 
 
-# ---------------------------------------------------------------
+# ---------------------------------------------------------------    # START BY LOADING cellchat_XXX_groupedClusters.RData (already analyzed versions)
 # For combining multiple datasets (SNCA + SNCA_PLX)
 # ---------------------------------------------------------------
 
 data.dir <- './Results/CellChat'
 setwd(data.dir)
+
+cellChat_SNCA <- netAnalysis_computeCentrality(cellChat_SNCA)
+cellChat_PLX<- netAnalysis_computeCentrality(cellChat_PLX)
 
 # Merge cellchat objects of each condition
 object.list <- list(SNCA = cellChat_SNCA, PLX = cellChat_PLX)
@@ -123,25 +131,36 @@ gg2 <- netVisual_heatmap(cellchat, measure = "weight")
 gg1 + gg2
 
 # Simplify Groups
-group.cellType <- c('CD8','CD8','NK','Treg','Treg','CD4','CD4','ProliferT',
-                    'Plasma','Plasma','Plasma','BCell','BCell','BCell','Endothilial','Proliferating','Dendritic','Dendritic','Dendritic',
-                    'Microglia18','Microglia','Microglia','Microglia','Microglia','Macrophage29','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph',
-                    'Oligo/Astro','Oligo/Astro','Astrocytes','Astrocytes','Pericyte','Ependymal','NA','NA','NA','NA','NA')
-group.cellType <- factor(group.cellType, levels = unique(group.cellType))
-object.list <- lapply(object.list, function(x) {mergeInteractions(x, group.cellType)})
-cellchat <- mergeCellChat(object.list, add.names = names(object.list))
-
-weight.max <- getMaxWeight(object.list, slot.name = c("idents", "net", "net"), attribute = c("idents","count", "count.merged"))
-par(mfrow = c(1,2), xpd=TRUE)
-for (i in 1:length(object.list)) {
-  netVisual_circle(object.list[[i]]@net$count.merged, weight.scale = T, label.edge= T, edge.weight.max = weight.max[3], 
-                   edge.width.max = 12, title.name = paste0("Number of interactions - ", names(object.list)[i]))
-}
+# group.cellType <- c('CD8','CD8','NK','Treg','Treg','CD4','CD4','ProliferT',
+#                     'Plasma','Plasma','Plasma','BCell','BCell','BCell','Endothilial','Proliferating','Dendritic','Dendritic','Dendritic',
+#                     'Microglia18','Microglia','Microglia','Microglia','Microglia','Macrophage29','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph','Mo/Mph',
+#                     'Oligo/Astro','Oligo/Astro','Astrocytes','Astrocytes','Pericyte','Ependymal','NA','NA','NA','NA','NA')
+# group.cellType <- factor(group.cellType, levels = unique(group.cellType))
+# object.list <- lapply(object.list, function(x) {mergeInteractions(x, group.cellType)})
+# cellchat <- mergeCellChat(object.list, add.names = names(object.list))
+#
+# weight.max <- getMaxWeight(object.list, slot.name = c("idents", "net", "net"), attribute = c("idents","count", "count.merged"))
+# par(mfrow = c(1,2), xpd=TRUE)
+# for (i in 1:length(object.list)) {
+#   netVisual_circle(object.list[[i]]@net$count.merged, weight.scale = T, label.edge= T, edge.weight.max = weight.max[3],
+#                    edge.width.max = 12, title.name = paste0("Number of interactions - ", names(object.list)[i]))
+# }
 
 # Differential
-par(mfrow = c(1,2), xpd=TRUE)
-netVisual_diffInteraction(cellchat, weight.scale = T, measure = "count.merged", label.edge = T)
-netVisual_diffInteraction(cellchat, weight.scale = T, measure = "weight.merged", label.edge = T)
+# par(mfrow = c(1,2), xpd=TRUE)
+# netVisual_diffInteraction(cellchat, weight.scale = T, measure = "count.merged", label.edge = T)
+# netVisual_diffInteraction(cellchat, weight.scale = T, measure = "weight.merged", label.edge = T)
+
+
+# "Create circle plots for each cell type separately
+mat <- object.list$PLX@net$weight
+par(mfrow = c(3,6), xpd=TRUE, mai=c(1,0.1,0.1,0.1))
+for (i in 1:nrow(mat)){
+  mat2 <- matrix(0, nrow = nrow(mat), ncol = ncol(mat), dimnames = dimnames(mat))
+  mat2[i,] <- mat[i,]
+  groupSize <- as.numeric(table(cellchat@idents$PLX))
+  netVisual_circle(mat2, vertex.weight = groupSize, weight.scale = T, edge.weight.max = max(mat), title.name = rownames(mat)[i])
+}
 
 # Identify cell populations with significant changes in sending or receiving signals
 num.link <- sapply(object.list, function(x) {rowSums(x@net$count) + colSums(x@net$count)-diag(x@net$count)})
@@ -154,8 +173,8 @@ for (i in 1:length(object.list)) {
 #> Signaling role analysis on the aggregated cell-cell communication network from all signaling pathways
 patchwork::wrap_plots(plots = gg)
 
-gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "c18", signaling.exclude = 'MIF')
-gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "c29", signaling.exclude = 'MIF')
+gg1 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "CD8", signaling.exclude = 'MIF')
+gg2 <- netAnalysis_signalingChanges_scatter(cellchat, idents.use = "CD4", signaling.exclude = 'MIF')
 patchwork::wrap_plots(plots = list(gg1,gg2))
 
 
@@ -192,11 +211,11 @@ draw(ht1 + ht2, ht_gap = unit(0.5, "cm"))
 # ---------------------------------------------------------------------------------------------
 ##### Part III: Identify the up-gulated and down-regulated signaling ligand-receptor pairs
 
-netVisual_bubble(cellchat, sources.use = c('c29','c18'), targets.use = c('c1','c8'),  comparison = c(1, 2), angle.x = 45)
+netVisual_bubble(cellchat, sources.use = c('Mo/M\u03D5','PLX-M\u03D5','Microglia'), targets.use = c('CD8'),  comparison = c(1, 2), angle.x = 45)
 
-gg1 <- netVisual_bubble(cellchat, sources.use = c('c29','c18'), targets.use = c('c1','c8'),  comparison = c(1, 2), max.dataset = 2,
+gg1 <- netVisual_bubble(cellchat, sources.use = c('Mo/M\u03D5','PLX-M\u03D5','Microglia'), targets.use = c('CD8'),  comparison = c(1, 2), max.dataset = 2,
                         title.name = "Increased signaling in PLX", angle.x = 45, remove.isolate = T)
-gg2 <- netVisual_bubble(cellchat, sources.use = c('c29','c18'), targets.use = c('c1','c8'),  comparison = c(1, 2), max.dataset = 1,
+gg2 <- netVisual_bubble(cellchat, sources.use = c('Mo/M\u03D5','PLX-M\u03D5','Microglia'), targets.use = c('CD8'),  comparison = c(1, 2), max.dataset = 1,
                         title.name = "Decreased signaling in PLX", angle.x = 45, remove.isolate = T)
 gg1 + gg2
 
@@ -224,23 +243,26 @@ df <- findEnrichedSignaling(object.list[[2]], features = c("Spp1"), idents = c('
 
 ## Visualize the identified up-regulated and down-regulated signaling ligand-receptor pairs
 pairLR.use.up = net.up[, "interaction_name", drop = F]
-gg1 <- netVisual_bubble(cellchat, pairLR.use = pairLR.use.up, sources.use = c('c29','c18'), targets.use = c('c1','c8'), comparison = c(1, 2),
+gg1 <- netVisual_bubble(cellchat, pairLR.use = pairLR.use.up, sources.use = c('CD8'), targets.use = c('Mo/M\u03D5','PLX-M\u03D5','Microglia'), comparison = c(1, 2),
                         angle.x = 90, remove.isolate = T,title.name = paste0("Up-regulated signaling in ", names(object.list)[2]))
 pairLR.use.down = net.down[, "interaction_name", drop = F]
-gg2 <- netVisual_bubble(cellchat, pairLR.use = pairLR.use.down, sources.use = c('c29','c18'), targets.use = c('c1','c8'), comparison = c(1, 2),
+gg2 <- netVisual_bubble(cellchat, pairLR.use = pairLR.use.down, sources.use = c('CD8'), targets.use = c('Mo/M\u03D5','PLX-M\u03D5','Microglia'), comparison = c(1, 2),
                         angle.x = 90, remove.isolate = T,title.name = paste0("Down-regulated signaling in ", names(object.list)[2]))
 gg1 + gg2
 
 library(wordcloud)
-computeEnrichmentScore(net.down, species = 'human', variable.both = TRUE) # enriched in SNCA
-computeEnrichmentScore(net.up, species = 'human', variable.both = TRUE) # enriched in PLX
+computeEnrichmentScore(net.down, species = 'mouse', variable.both = TRUE) # enriched in SNCA
+computeEnrichmentScore(net.up, species = 'mouse', variable.both = TRUE) # enriched in PLX
 
 
 # ---------------------------------------------------------------------------------------------
 # Part IV: Visually compare cell-cell communication using Hierarchy plot, Circle plot or Chord diagram
 
+# pathways in both conditions
+intersect(object.list$SNCA@netP$pathways, object.list$PLX@netP$pathways)
+
 #circle plot
-pathways.show <- c("CD40") 
+pathways.show <- c("CD45") 
 weight.max <- getMaxWeight(object.list, slot.name = c("netP"), attribute = pathways.show) # control the edge weights across different datasets
 par(mfrow = c(1,2), xpd=TRUE)
 for (i in 1:length(object.list)) {
@@ -248,7 +270,7 @@ for (i in 1:length(object.list)) {
 }
 
 #heatmap
-pathways.show <- c("CCL") 
+pathways.show <- c("TGFb") 
 par(mfrow = c(1,2), xpd=TRUE)
 ht <- list()
 for (i in 1:length(object.list)) {
@@ -257,7 +279,7 @@ for (i in 1:length(object.list)) {
 ComplexHeatmap::draw(ht[[1]] + ht[[2]], ht_gap = unit(0.5, "cm"))
 
 # Chord diagram
-pathways.show <- c("CADM") 
+pathways.show <- c("SEMA6") 
 par(mfrow = c(1,2), xpd=TRUE)
 for (i in 1:length(object.list)) {
   netVisual_aggregate(object.list[[i]], signaling = pathways.show, layout = "chord", signaling.name = paste(pathways.show, names(object.list)[i]))
--- Scripts/heatmapFromExcel.R
+++ Scripts/heatmapFromExcel.R
@@ -1,5 +1,8 @@
-library(openxlsx)
 library(pheatmap)
+library(readxl)
+library(viridis)
+library(ggplot2)
+library(tidyverse)
 
 dea_counts_matrix <- as.data.frame(read_xlsx('data/DEA_SNCA_vs_WT_Giosue.xlsx'))
 
@@ -9,16 +12,38 @@ dea_counts_matrix$...1 <- NULL
 
 
 features_of_interest <- c('Gfap','Vim','Serping1','Ggta1','Iigp1','Gbp2','Fbln5','Fkbp5','Psmb8','Srgn')
+features_of_interest <- c('Ccl8','Ccl4','Ccl7','Ccl5','Cxcl10','Cxcl13','Cxcl16','Ccl3','Ccl2',
+                          'Cx3cl1','Ccl1')
+
 
 dea_counts_subset <- dea_counts_matrix[features_of_interest,]
 
 ComplexHeatmap::pheatmap(as.matrix(dea_counts_subset),
          scale = "row",
-         color = rev(RColorBrewer::brewer.pal(n =4, name = "RdBu")),
+         #color = rev(RColorBrewer::brewer.pal(n =4, name = "RdBu")),
+         color = inferno(256),
          #color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
          main = "Heatmap of Gene Expression",
          cluster_rows=FALSE, cluster_cols=FALSE,
-         column_names_side=c('top'), angle_col = c('45'),
+         column_names_side=c('top'), angle_col = c('0'),
          row_names_side=c('left'),
+         heatmap_legend_param = list(title = NULL)
          )
+
+# Heatmap with ggplot
+dea_counts_subset_scaled <- t(scale(t(dea_counts_subset)))
+dea_counts_subset_modified <- data.frame(Gene=rownames(dea_counts_subset_scaled),dea_counts_subset_scaled)
+dea_counts_subset_long <- pivot_longer(dea_counts_subset_modified, cols = -Gene)
+dea_counts_subset_long$Gene <- factor(dea_counts_subset_long$Gene, levels = rev(features_of_interest))  # set genes as factor to order the rows
+
+ggplot(dea_counts_subset_long, aes(x=name, y=Gene, fill=value)) +
+  geom_tile(width=0.99, height=0.99) +
+  scale_fill_viridis(option='rocket', discrete=F) +
+  #scale_fill_distiller(palette = "PuOr") +
+  theme_minimal() +
+  theme(text=element_text(size=20), legend.title=element_blank(), legend.key.height = unit(2, "cm")) +
+  scale_x_discrete(position = "top") +
+  labs(x=NULL,y=NULL)
+
+
 
